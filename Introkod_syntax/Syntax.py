5218b513beb31866f3651742b0b87af5c87bb810
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-08-28 09:37:29 +0200

    Intro

---
+++ Introkod_syntax/Syntax.py
@@ -0,0 +1,12 @@
+print("hejsan")
+
+# skapar en var
+name = "Liam"
+
+print(f"ditt namn är {name}")
+#med en format print så kan man printa variabler i meningar
+
+side = float(input("Ange kvadratens sida: "))
+area = side**2
+
+print(f"arean av kvadraten är {area} a.e.")
