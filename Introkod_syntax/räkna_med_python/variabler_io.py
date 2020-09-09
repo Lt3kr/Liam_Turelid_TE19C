5e17a11791a356daf3964d84825e36cf7f9d4e30
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-09-09 13:59:41 +0200

    fixade lila delar p.g.a fel placering av a tag

--- Introkod_syntax/räkna_med_python/variabler_io.py
+++ Introkod_syntax/räkna_med_python/variabler_io.py
@@ -3,3 +3,25 @@ K = float(input("konvertera kelvin till celsius: "))
 C = K-273
 
 print(f"talet du söker är {C} grader celsius")
+
+c = float(input("nu kan du konvertera celsius till kelvin: "))
+
+k = c+273
+
+print(f"det blir {k} kelvin")
+
+#Eb = engångsbiljeter Mk = månadskort
+
+Eb = 30
+Mk = 775
+
+H = float(input("hur mycket åker du?: "))
+
+f = 30*H
+
+if (f >= Mk): {
+    print(f"Du borde nog överväga ett månadskort för du ligger på {f}kr och ett månadskort är endast 775kr")
+}
+
+else: 
+    print(f" det kostar dig {f} kr att åka så mycket")
