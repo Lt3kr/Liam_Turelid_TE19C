b4722cf8a3b0b4b7d4528668edb4ab75bda6d830
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-10-07 14:09:05 +0200

    nära att göra klart feman

--- Introkod_syntax/While läxor/While_läxa_2.py
+++ Introkod_syntax/While läxor/While_läxa_2.py
@@ -1,4 +1,7 @@
-s=0
+s=1
 i=0
 
-while 
+while s <= 99:
+    i = i+s
+    s += 2
+    print(i)
---
+++ Introkod_syntax/While läxor/While_läxa_5.py
@@ -0,0 +1,8 @@
+#gör ett program som använder sig av ett flödesschema
+
+tal = 54
+
+
+n = input("thee needs to guess my number from one to one hundred: ")
+while n != tal:
+    print("you are not correct try again")
