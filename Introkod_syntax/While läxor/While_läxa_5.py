537cc9415e44af30bcf462f7bb5ee9edfeb40050
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-10-09 09:38:13 +0200

    gjorde uppgift 5

--- Introkod_syntax/While läxor/While_läxa_5.py
+++ Introkod_syntax/While läxor/While_läxa_5.py
@@ -1,8 +1,20 @@
 #gör ett program som använder sig av ett flödesschema
+import random
 
-tal = 54
+
+tal = random.randint(1,100) #ger variabeln tal ett random tal från 1-100
 
 
-n = input("thee needs to guess my number from one to one hundred: ")
-while n != tal:
-    print("you are not correct try again")
+n = float(input("thee needs to guess my number from one to one hundred: ")) #frågar efter talet
+
+while n != tal: #när gissningen inte är korrekt så startas while satsen
+    if n < tal:
+        print(f"Thy are below what I seek, to be specifik you are {tal-n} away") #if satsen kollar om du är under talet och säger även hur långt under
+    elif n > tal:
+        print(f"Thy are abowe what I seek, to be specifik you are {n-tal} away")
+    n = float(input("Now try again: ")) #den här elif satsen gör tvärtom vad if satsen gjorde
+
+print("you are indeed correct") #när while loopen inte längre är aktiv så printas detta
+
+
+#jag måste tacka Nils i min klass då han hjälpte mig förstå denna uppgift
