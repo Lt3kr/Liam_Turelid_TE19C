3b18cafd2bd3eb2c96fde1f5ec2b654f0ad63886
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-12-11 09:16:28 +0100

    Gjorde uppgift 3

--- Introkod_syntax/FunktionsUppgifter/uppgift3.py
+++ Introkod_syntax/FunktionsUppgifter/uppgift3.py
@@ -1,8 +1,14 @@
 # punkt 1 = 3,3 och punkt 2 = -2,1
 
 #vad är avståndet från origon, det är pythagoras sats
+import math
 
-def sammanlagda_avståndet(Avstånd1_Avstånd2):
-    
+Punkt = [3,3]
+Punkt2 = [-2,1]
 
+def avståndet(Punkt, Punkt2):
+    return ((Punkt[0] - Punkt2[0])**2 + (Punkt[1] - Punkt2[1])**2)
 
+sanna_avståndet = math.sqrt(avståndet(Punkt, Punkt2))
+
+print(f"distans ifrån punkterna är {sanna_avståndet}")
