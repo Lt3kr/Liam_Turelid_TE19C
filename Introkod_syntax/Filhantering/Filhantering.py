9792e46ed59a312138f4196af8dc7ad50b0dc847
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-03 09:40:20 +0100

    gjorde dicRoll

--- Introkod_syntax/Filhantering/Filhantering.py
+++ Introkod_syntax/Filhantering/Filhantering.py
@@ -3,11 +3,19 @@ import random as rnd
 allaKast = []
 HurMångaFemmor = 0
 
-with open("diceRoll.txt", "w"):
-    for i in range(10):
-        tärningskast = rnd.randint(1, 6)
-        allaKast.append(tärningskast)
-    print(allaKast)
-    print(sorted(allaKast))
-    for i in range(allaKast.__len__):
-    
+for i in range(10):
+    tärningskast = rnd.randint(1, 6)
+    allaKast.append(tärningskast)
+print(allaKast)
+print(sorted(allaKast))
+for i in allaKast:
+    if i == 5:
+        HurMångaFemmor += 1
+print(HurMångaFemmor)
+
+
+with open("diceRoll.txt", "w") as diceRoll:
+    diceRoll.write("Alla kasten " + str(allaKast) + "\n")
+    diceRoll.write("De sorterade kasten " + str(sorted(allaKast)) + "\n")
+    diceRoll.write("Hur många femmor " + str(HurMångaFemmor))
+    diceRoll.close()
