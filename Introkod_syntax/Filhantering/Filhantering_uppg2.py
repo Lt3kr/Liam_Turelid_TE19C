1bae71aab53a54d616d4ce1ad721ba7156bb7cda
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-05 09:37:07 +0100

    Försöker mig på C

--- Introkod_syntax/Filhantering/Filhantering_uppg2.py
+++ Introkod_syntax/Filhantering/Filhantering_uppg2.py
@@ -1,9 +1,33 @@
 with open("Provresultat.txt", "r") as f1:
-    a = sorted(f1)
+    a = sorted(open("Provresultat.txt"))
 
-with open("Provresultat.txt", "a") as f2:
-    for i in f2:
-        f2.write(str(a))
-        f2.write("\n")
+#with open("Provresultat.txt", "a") as f2:
+    #f2.write("\n")
+    #for name in a:
+        #f2.write(str(name))
+
+with open("Provresultat.txt", "a") as f3:
+    A = []
+    B = []
+    C = []
+    D = []
+    E = []
+    F = []
+    for gamer in a:
+        names = gamer.split("\n")
+        poäng = int(names[-2:])
+        if poäng < 20:
+            F.append(names)
+        elif poäng < 30:
+            E.append(names)
+        elif poäng < 40:
+            D.append(names)
+        elif poäng < 50:
+            C.append(names)
+        elif poäng < 60:
+            B.append(names)
+        elif poäng < 70:
+            A.append(names)
+    f3.write("\n" + "F:" + "\n" + str(F) + "\n" + "E:" + "\n"+ str(E) + "\n" + "D:" + "\n" + str(D) + "\n" + "C:" + "\n" + str(C) + "\n" + "B:" + "\n" + str(B) + "\n" + "A:" + "\n" + str(A))
 
 
