448ebf03522172117b91bd84ff35dc08f364220e
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-12 09:40:17 +0100

    fortsättning på uppgift 2 och skapade uppgift 3

--- Introkod_syntax/Dictonary/uppgift2.py
+++ Introkod_syntax/Dictonary/uppgift2.py
@@ -1,16 +1,14 @@
 import random as rnd
 
-x = 0
-
-for i in range(100000):
-    tärningskast = rnd.randint(1, 6)
-    if tärningskast == 1:
-        x += 1
-    elif tärningskast == 2:
-        x += 1
-
-Olikatärningskast = {
-    
-}
-
-print(x)
+antalsiffror = {}
+for i in range(1,6):
+    antalsiffror[i:] = 0 
+    for i in range(100000):
+        tärningskast = rnd.randint(1, 6)
+        if tärningskast == 1:
+            antalsiffror[1:] += 1
+        elif tärningskast == 2:
+            antalsiffror[2:] += 1
+print(f"Så här många tvåor och ettor var det: ")
+for key in antalsiffror:
+    print(key, antalsiffror[key])
---
+++ Introkod_syntax/Dictonary/uppgift3.py
@@ -0,0 +1,4 @@
+
+
+pokeindex = {}
+
