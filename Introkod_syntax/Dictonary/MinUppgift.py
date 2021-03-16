03340606b7ef7cc57f6ccea25a1e57eb93df9ee9
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-16 11:53:47 +0100

    Börjar lite på eget

---
+++ Introkod_syntax/Dictonary/MinUppgift.py
@@ -0,0 +1,11 @@
+#Gör en dictionary som föreslår vad du ska göra utefter vart du beffiner dig i Terraria och vilken klass.
+VilkenDel = {
+    "Pre-Hardmode": {"summoner":["1:Obsidian armour, Snapthorn, Impstaff, Ballista Rod, Summoning potion, witching table, Pygmy Necklace, Feral Claws, Shield of Chtulhu, Worm scarf, Brain of confusion, Terraspark 2:"]},
+    "Hardmode": {},
+    "Pre-Mech": {},
+    "Pre-Planterra": {},
+    "Pre-Golem":{},
+    "Pre-Lunar": {},
+    "Pre-Moonlord": {}
+}
+#Gör även en function som säger vad man behöver för att skapa olika grejer
--- Introkod_syntax/Dictonary/uppgift2.py
+++ Introkod_syntax/Dictonary/uppgift2.py
@@ -1,14 +1,25 @@
 import random as rnd
 
 antalsiffror = {}
-for i in range(1,6):
-    antalsiffror[i:] = 0 
+for i in range(1, 7):
+    antalsiffror[i] = 0 
+
     for i in range(100000):
         tärningskast = rnd.randint(1, 6)
         if tärningskast == 1:
-            antalsiffror[1:] += 1
+            antalsiffror[1] += 1
         elif tärningskast == 2:
-            antalsiffror[2:] += 1
-print(f"Så här många tvåor och ettor var det: ")
-for key in antalsiffror:
-    print(key, antalsiffror[key])
+            antalsiffror[2] += 1
+        elif tärningskast == 3:
+            antalsiffror[3] += 1
+        elif tärningskast == 4:
+            antalsiffror[4] += 1
+        elif tärningskast == 5:
+            antalsiffror[5] += 1
+        elif tärningskast == 6:
+            antalsiffror[6] += 1
+
+for siffror in antalsiffror:
+    print(f"{siffror}: {antalsiffror[siffror]}")
+
+#Viking hjälpte mig att förstå och det är därför det kan finnas liknelser
