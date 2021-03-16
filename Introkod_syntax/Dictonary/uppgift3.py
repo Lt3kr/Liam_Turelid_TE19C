47dc027a2d729959985f37abf799e30971168cd9
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-16 12:58:47 +0100

    Jag gjorde uppgift 2 men klarade inte uppg.3

--- Introkod_syntax/Dictonary/uppgift3.py
+++ Introkod_syntax/Dictonary/uppgift3.py
@@ -1,2 +1,6 @@
-pokeindex = {}
+with open("pokemonlista.txt", "r") as f1:# öppna filen
 
+    #gör den till en lista med hjälp av split() och sätt siffrorna längst bak genom att byta position
+
+    #sätt in listan i pokeindex och gör det till en dict
+    
