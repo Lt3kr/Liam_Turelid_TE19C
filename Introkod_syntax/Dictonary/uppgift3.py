1eb3c70109bfb7433dbdaadfda687eb1ab6c9e1a
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-17 14:04:16 +0100

    Gjorde uppgift 3

--- Introkod_syntax/Dictonary/uppgift3.py
+++ Introkod_syntax/Dictonary/uppgift3.py
@@ -5,7 +5,7 @@ with open("pokemonlista.txt", "r") as f1:
     for rad in f1:
         rad = rad.strip("\n")
         rad = rad.split()
-        pokedex [rad[1]] = rad[0], rad[2]
+        pokedex [rad[1]] = (f"index: {rad[0]} typ: {rad[2]}")
             
 
 print(pokedex["Mewtwo"])
