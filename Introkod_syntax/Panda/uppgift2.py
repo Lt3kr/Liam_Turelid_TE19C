2123ef600692d742fd253951ca510e79fda7acbb
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-04-21 13:47:59 +0200

    Klar med uppgift 2

--- Introkod_syntax/Panda/uppgift2.py
+++ Introkod_syntax/Panda/uppgift2.py
@@ -2,10 +2,34 @@
 # import pandas as pd
 # import matplotlib.pyplot as plt
 
-# sum1 = 0
-# sum2 = 0
+# df = pd.read_csv("sysselsattning.csv", encoding = "ISO-8859-1", header=1)
+# ef = df[df["sysselsättning"] == "förvärvsarbetande"]
+# män = ef[ef["kön"] == "män"]["2019"].sum()
+
+#b)
 
 # df = pd.read_csv("sysselsattning.csv", encoding = "ISO-8859-1", header=1)
 # ef = df[df["sysselsättning"] == "förvärvsarbetande"]
-# k = ef[df["kön"] == "män"]
-# summa = k["2019"].sum()
+# kvinnor = ef[ef["kön"] == "kvinnor"]["2019"].sum()
+
+#c)
+
+# df = pd.read_csv("sysselsattning.csv", encoding = "ISO-8859-1", header=1)
+# ef = df[df["sysselsättning"] == "ej förvärvsarbetande"]
+# ej_män = ef[ef["kön"] == "män"]["2019"].sum()
+
+#d)
+
+# df = pd.read_csv("sysselsattning.csv", encoding = "ISO-8859-1", header=1)
+# ef = df[df["sysselsättning"] == "ej förvärvsarbetande"]
+# ej_kvinnor = ef[ef["kön"] == "kvinnor"]["2019"].sum()
+
+#e)
+
+#label = ["Män förs.", "Män ej förs.", "Kvinnor förs.", "Kvinnor ej förs."]
+#värde = [män, ej_män, kvinnor, ej_kvinnor]
+#x_axel = np.arange(len(label))
+
+#plt.title("Förvärvsarbetande")
+#plt.ylabel("Miljoner")
+#plt.bar(x_axel, värde)
