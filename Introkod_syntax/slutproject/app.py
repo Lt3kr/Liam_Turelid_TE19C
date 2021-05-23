2a5efd4464bb0ddf054f3bdceef2603bd3fee8b6
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-05-23 16:29:40 +0200

    Min magnum opus. Detta är vad jag kommer ihågs för

--- Introkod_syntax/slutproject/app.py
+++ Introkod_syntax/slutproject/app.py
@@ -1,6 +1,8 @@
 from logging import PlaceHolder
 from re import X
 from dash_core_components.Graph import Graph
+from dash_html_components.Data import Data
+from dash_html_components.Label import Label
 import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
@@ -14,27 +16,25 @@ import plotly.graph_objects as go
 
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__) # starten på själva servern
 
-app = dash.Dash(__name__)
+Regional = pd.read_csv("Regional_Totals_Data.csv", encoding = "UTF-8") # läser in Regional_Totals_Data.csv filen så datorn kan använda informationen
+Gender = pd.read_csv("Gender_Data.csv", encoding = "UTF-8") # läser in Gender_Data.csv filen så datorn kan använda informationen
 
-Regional = pd.read_csv("Regional_Totals_Data.csv", encoding = "UTF-8")
-Gender = pd.read_csv("Gender_Data.csv", encoding = "UTF-8")
-
-antaletFallKön = px.bar(Gender, x="Gender", y="Total_Cases", title="Alla antal fall")
-dödaKön = px.pie(Gender, values="Total_Deaths", names="Gender", title="Procent av döda män och kvinnor")
+antaletFallKön = px.bar(Gender, x="Gender", y="Total_Cases", title="Alla antal fall") # detta är själva antaletFallKön statiska grafen som jag kallar på i layouten
+dödaKön = px.pie(Gender, values="Total_Deaths", names="Gender", title="Procent av döda män och kvinnor") # detta är den statiska dödakön grafen jag kallar på i layouten
 
 app.layout = html.Div(children=[
     html.H1(children="Welcome to Liams graphs",
     style={
         'textAlign': 'center',
         'color': '#000000'
-    }),
+    }), # endast en text div med styling i
 
-    html.Div(children='''
+    html.Div(children=''' 
     Skriv in i sökfälten vilka städer du vill jämföra glöm inte att inte stava fel och med stora bokstäver
-    '''),
-    html.Div([   #Detta är inputfältet som man ska skriva in de städer som man vill ha
+    '''), # detta är endast en text div
+    html.Div([   #Detta är inputfältet som man ska skriva in de städer som man vill ha att jämföra med
         dcc.Input(
         id="stad_1",
         type='text',
@@ -57,43 +57,107 @@ app.layout = html.Div(children=[
         )
     ]),
 
-    html.Datalist(id='stadVal', children=[ #Skapar en datalist som innehåller förbestämda val som man kan klicka in omedelbart
+    html.Div(children='''Vad vill du undersöka?'''),
+
+    dcc.Input( # detta är en text input för att man ska välja vad man vill jämföra
+        id="JämförVal",
+        type="text",
+        list="undersökningVal"
+    ),
+
+    dcc.Graph( # detta är den responsiva stapel grafen för valda städer
+    id="StadGraf"
+    ),
+
+    dcc.Graph( # detta är den responsiva pie grafen för valda städer
+        id="StadGraf2"
+    ),
+
+    html.Datalist(id='stadVal', children=[ #Skapar en datalist som innehåller förbestämda val som man kan klicka in omedelbart för städer
         html.Option(value='Blekinge'),
         html.Option(value='Dalarna'),
         html.Option(value='Gotland'),
-        html.Option(value='Gävleborg')
+        html.Option(value='Gävleborg'),
+        html.Option(value='Halland'),
+        html.Option(value='Jämtland Härjedalen'),
+        html.Option(value='Jönköping'),
+        html.Option(value='Kalmar'),
+        html.Option(value='Kronoberg'),
+        html.Option(value='Norrbotten'),
+        html.Option(value='Skåne'),
+        html.Option(value='Stockholm'),
+        html.Option(value='Sörmland'),
+        html.Option(value='Uppsala'),
+        html.Option(value='Värmland'),
+        html.Option(value='Västerbotten'),
+        html.Option(value='Västernorrland'),
+        html.Option(value='Västmanland'),
+        html.Option(value='Västra Götaland'),
+        html.Option(value='Örebro'),
+        html.Option(value='Östergötland')
     ]),
 
-    dcc.Graph(
+    html.Datalist(id='undersökningVal', children=[ #Skapar en datalist som innehåller förbestämda val som man kan klicka in omedelbart för vad man vill jämföra
+        html.Option(label= "antalet döda", value='Total_Deaths'),
+        html.Option(label= "antalet fall", value='Total_Cases'),
+        html.Option(label= "antal fall per 100k", value='Cases_per_100k_Pop'),
+        html.Option(label= "antal ICU", value='Total_ICU_Admissions'),
+    ]),
+
+    dcc.Graph( # gör en statisk graf utav antaletFallKön charten
         id= "könsGraf",
         figure = antaletFallKön
     ),
 
-    dcc.Graph(
+    dcc.Graph( # gör en statisk graf utav dödaKön charten
         id="dödKön",
         figure = dödaKön
     ),
 
 ])
 
-@app.callback(
-    Output("könsGraf","dödKön"),
+@app.callback( # gör en callback för att funktionen ska få alla värden och sedan presenteras
+    Output("StadGraf","figure"),
     [Input("stad_1", "value")],
     [Input("stad_2", "value")],
     [Input("stad_3", "value")],
-    [Input("stad_4", "value")]
+    [Input("stad_4", "value")],
+    [Input("JämförVal", "value")]
 )
 
-def update_figure(stad_1, stad_2, stad_3, stad_4):
+def update_figure(stad_1, stad_2, stad_3, stad_4, JämförVal): # gör en funktion för att det inte ska vara statiska grafer
 
-    städer = [stad_1,stad_2,stad_3,stad_4]
+    städer = [stad_1,stad_2,stad_3,stad_4] # gör en array för enkelhetens skull
 
-    df = Regional[(Regional["Region"].isin([städer]))]
+    dataframe = Regional[(Regional["Region"].isin([stad_1,stad_2,stad_3,stad_4]))] # skapar en ny dataframe, gör detta av den gamla och använder .isin funktionen för att hitta de val användaren vill ha
 
-    fig = {'data': [dict(x=städer , y = df["Total_Deaths"] ,type="bar")],
+    fig = {'data': [dict(x=städer , y = dataframe[JämförVal] ,type="bar")], # skapar en responsiv graf baserad på inputsen av både städer och vad man vill jämföra i form av JämförVal
 
-        'layout':dict(title= "snälla funka")}
+        'layout':dict(title= "Resultaten: ")}
     return fig
+
+@app.callback( # gör samma callback för den andra funktionen så den tar in samma värden som den andra
+    Output("StadGraf2","figure"),
+    [Input("stad_1", "value")],
+    [Input("stad_2", "value")],
+    [Input("stad_3", "value")],
+    [Input("stad_4", "value")],
+    [Input("JämförVal", "value")]
+)
+
+def update_figure2(stad_1, stad_2, stad_3, stad_4, JämförVal): # gör en ny funktion för min pie chart 
+
+    städer = [stad_1,stad_2,stad_3,stad_4] # lägger all städer i en array för atta användas senare i min dataframe och i min graf
+
+    dataframe = Regional[(Regional["Region"].isin([stad_1,stad_2,stad_3,stad_4]))] # Gör ännu en gång en ny dataframe för enkelhetens skull
+
+    fig = go.Figure(data=[go.Pie(labels=städer, values=dataframe[JämförVal])]) # Gör en pie chart som tar in värdena städer som labels och min datatframe med val av JämförVal för values
+
+    return fig
+
 
 if __name__=="__main__":
     app.run_server(debug=True)
+
+
+    
